6f07242dbafe2a77f7a64d154ae2964f3de87802
JojoS62 <sn2010@onlinehome.de>
2020-10-03 14:46:12 +0200

    update to last changes in cortexm_romtable

--- src/target/adiv5.h
+++ src/target/adiv5.h
@@ -87,10 +87,14 @@
 #define ADIV5_AP_IDR		ADIV5_AP_REG(0xFC)
 
 /* Known designers seen in SYSROM-PIDR. Ignore Bit 7 from the designer bits*/
-#define AP_DESIGNER_TEXAS 0x17
-#define AP_DESIGNER_ATMEL 0x1f
-#define AP_DESIGNER_STM 0x20
-#define AP_DESIGNER_ARM 0x43b
+#define AP_DESIGNER_FREESCALE    0x00e
+#define AP_DESIGNER_TEXAS        0x017
+#define AP_DESIGNER_ATMEL        0x01f
+#define AP_DESIGNER_STM          0x020
+#define AP_DESIGNER_CYPRESS      0x034
+#define AP_DESIGNER_ARM          0x43b
+/*LPC845 with designer 501. Strange!? */
+#define AP_DESIGNER_SPECULAR     0x501
 #define AP_DESIGNER_ENERGY_MICRO 0x673
 
 /* AP Control and Status Word (CSW) */
