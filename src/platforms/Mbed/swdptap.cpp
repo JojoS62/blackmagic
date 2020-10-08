5fe29c1625013ac016b988b90dc29b29a5e06ea4
JojoS62 <sn2010@onlinehome.de>
2020-10-08 19:37:38 +0200

    test single NOP for faster SWD

--- src/platforms/Mbed/swdptap.cpp
+++ src/platforms/Mbed/swdptap.cpp
@@ -96,9 +96,9 @@ DigitalInOut swdDIO(PB_8, PIN_OUTPUT, PullNone, 1);
 constexpr uint16_t DELAY_CYCLES = 1;
 void clockDelay(uint16_t n) 
 {
-	while(--n) {
+	// while(--n) {
 		__NOP();
-	};
+	// };
 }
 
 static void swdptap_turnaround(int dir)
